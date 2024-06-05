19e563574072c15e463f51d6da4c971bf8997578
Carolina Ekdal <Carolina.ekdal@gmail.com>
2024-06-05 12:45:48 +0200

    skapa komponent

---
+++ 0-Skapa-komponent/src/components/FirstComponent.tsx
@@ -0,0 +1,14 @@
+export const FirstComponent = () => {
+  let x = 17;
+  let y = 4;
+
+  const calculate = () => {
+    let result = x + y;
+    return result;
+  };
+  return (
+    <>
+      <div id="div"></div>
+    </>
+  );
+};
